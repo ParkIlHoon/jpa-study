605190e7500fc5e193adf64d32e83c0688b5830c
1hoon <chiwoo2074@gmail.com>
2020-07-21 22:12:36 +0900

    상품 repository, service 작성

--- spring-boot-jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ spring-boot-jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -27,4 +28,31 @@ public abstract class Item
 	@ManyToMany
 	@JoinTable(name = "category_item", joinColumns = @JoinColumn(name = "item_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private List<Category> categories = new ArrayList();
+
+
+	/*******************************************************************************************************************
+	 * 비즈니스 로직
+	 ******************************************************************************************************************/
+	/**
+	 * 재고 수량 증가
+	 * @param quantity 증가량
+	 */
+	public void addStock(int quantity)
+	{
+		this.stockQuantity += quantity;
+	}
+
+	/**
+	 * 재고 수량 감소
+	 * @param quantity 감소량
+	 */
+	public void removeStock(int quantity)
+	{
+		int restStock = this.stockQuantity - quantity;
+		if (restStock < 0)
+		{
+			throw new NotEnoughStockException("need more stock");
+		}
+		this.stockQuantity = restStock;
+	}
 }
---
+++ spring-boot-jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,54 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository
+{
+	private final EntityManager entityManager;
+
+	/**
+	 * 상품을 저장한다. 
+	 * @param item 저장할 상품
+	 */
+	public void save(Item item)
+	{
+		// 신규 데이터
+		if (item.getId() == null)
+		{
+			entityManager.persist(item);
+		}
+		// 기존 데이터
+		else
+		{
+			entityManager.merge(item);
+		}
+	}
+
+	/**
+	 * 하나의 상품을 조회한다.
+	 * @param id 조회할 상품의 아이디
+	 * @return 아이디에 해당하는 상품
+	 */
+	public Item findOne(Long id)
+	{
+		return entityManager.find(Item.class, id);
+	}
+
+	/**
+	 * 전체 상품 목록을 조회한다.
+	 * @return 전체 상품 목록
+	 */
+	public List<Item> findAll()
+	{
+		return entityManager.createQuery("select i from Item i", Item.class).getResultList();
+	}
+
+
+}
