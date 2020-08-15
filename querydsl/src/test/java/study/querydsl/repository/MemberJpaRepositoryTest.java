96c9d4edf77bbae6cb7086f3f9950ad1dc298a85
ParkIlHoon <chiwoo2074@gmail.com>
2020-08-15 17:00:05 +0900

    where절 파라미터 사용

--- querydsl/src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
+++ querydsl/src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -109,4 +109,36 @@ class MemberJpaRepositoryTest
         // then
         assertThat(result).extracting("username").containsExactly("member4");
     }
+
+    @Test
+    void 쿼리dsl()
+    {
+        // given
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+
+        entityManager.persist(teamA);
+        entityManager.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        entityManager.persist(member1);
+        entityManager.persist(member2);
+        entityManager.persist(member3);
+        entityManager.persist(member4);
+
+        // when
+        MemberSearchCondition condition = new MemberSearchCondition();
+        condition.setAgeGoe(35);
+        condition.setAgeLoe(40);
+        condition.setTeamName("teamB");
+
+        List<MemberTeamDto> result = memberJpaRepository.search(condition);
+
+        // then
+        assertThat(result).extracting("username").containsExactly("member4");
+    }
 }
