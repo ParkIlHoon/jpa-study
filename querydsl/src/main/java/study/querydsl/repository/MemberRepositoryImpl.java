a146b93c70e59709bb2e63e943b239cdd552a426
ParkIlHoon <chiwoo2074@gmail.com>
2020-08-16 18:02:02 +0900

    페이징 최적화

--- querydsl/src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ querydsl/src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -3,13 +3,16 @@ package study.querydsl.repository;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.support.PageableExecutionUtils;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.dto.QMemberTeamDto;
+import study.querydsl.entity.Member;
 
 import javax.persistence.EntityManager;
 import java.util.List;
@@ -92,7 +95,8 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom
                 .limit(pageable.getPageSize())
                 .fetch();
 
-        long count = queryFactory.select(member)
+        // 페이징 쿼리 최적화 -> content 사이즈가 페이지 사이즈보다 작을 땐 count 쿼리를 실행시키지 않는다.
+        JPAQuery<Member> jpaQuery = queryFactory.select(member)
                 .from(member)
                 .leftJoin(member.team, team)
                 .where(
@@ -100,10 +104,10 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom
                         teamNameEq(condition.getTeamName()),
                         ageGoe(condition.getAgeGoe()),
                         ageLoe(condition.getAgeLoe())
-                )
-                .fetchCount();
+                );
 
-        return new PageImpl<>(fetch, pageable, count);
+//        return new PageImpl<>(fetch, pageable, count);
+        return PageableExecutionUtils.getPage(fetch, pageable, () -> jpaQuery.fetchCount());
     }
 
     private BooleanExpression ageLoe(Integer ageLoe)
