f302dd293cc7e3a4faa529184339cf58e3cea77e
ParkIlHoon <chiwoo2074@gmail.com>
2020-09-20 14:53:33 +0900

    예제 도메인 모델 생성

--- data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -1,26 +1,42 @@
 package study.datajpa.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(exclude = {"team"})
 public class Member
 {
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
     private String username;
-
-    protected Member()
-    {
-    }
+    private int age;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     public Member(String username)
     {
         this.username = username;
+    }
+
+    public Member(String name, int age, Team team)
+    {
+        this.username = name;
+        this.age = age;
+        if (team != null)
+        {
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam (Team team)
+    {
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
