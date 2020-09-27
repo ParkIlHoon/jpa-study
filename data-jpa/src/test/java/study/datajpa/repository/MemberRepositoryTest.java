87b2b3caa5b34a840fa89f43a8dad1e6f3a8d5cf
ParkIlHoon <chiwoo2074@gmail.com>
2020-09-27 16:19:28 +0900

    파라미터 바인딩

--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -45,5 +46,21 @@ class MemberRepositoryTest
         assertThat(result.get(0).getUsername()).isEqualTo("1hoon");
         assertThat(result.get(0).getAge()).isGreaterThan(15);
         assertThat(result.size()).isEqualTo(1);
+    }
+
+    @Test
+    void findByNamesTest()
+    {
+        Member member1 = new Member("AAA", 10, null);
+        Member member2 = new Member("BBB", 20, null);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        List<Member> result = memberRepository.findByNames(Arrays.asList("AAA", "BBB"));
+
+        assertThat(result.get(0).getUsername()).isEqualTo("AAA");
+        assertThat(result.get(1).getUsername()).isEqualTo("BBB");
+        assertThat(result.size()).isEqualTo(2);
     }
 }
